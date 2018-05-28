e6c22c25a3f5c0dcf42d88bf9aad62aad015df9f
Matthew <matt@brians.com.au>
2018-05-29 09:41:53 +1000

    Planned Stuff
    Planned roughly what would be happening in the added documentation

--- main/DataFinder.java
+++ main/DataFinder.java
@@ -17,8 +17,18 @@ public class DataFinder {
 		do {
 			if (getString("Do you want to prescout a Team or an Event?", "Team", "Event").equals("Team")) {
 				getString("What is the number of the Team you wish to prescout?");
+				//Print potential statistics and asks for input until nothing is entered to decided stats to analyse
+				//Go through every qualification match they played this season and average their score for selected stats
+				//Prints data pulling status while doing above
+				//Ask for where to save the data as a CSV
+				//Save the data as a CSV in the given location
 			} else {
-				
+				//Print potential statistics and asks for input until nothing is entered to decided stats to analyse
+				//Go through every qualification match every team in this comp played this season and average their 
+				//score for selected stats
+				//Prints data pulling status while doing above
+				//Ask for where to save the data as a CSV
+				//Save the data as a CSV in the given location
 			}
 		} while (getString("Would you like to prescout more?", "Yes", "No").equals("Yes"));
 			
